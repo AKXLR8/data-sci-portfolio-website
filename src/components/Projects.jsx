7c03822867765f272702e2912087a1a24d0e9af3
Akshay Maniyampara <akshaymaniyampara40@gmail.com>
2026-05-15 00:43:16 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ const projects = [
     title: "NYC Uber Trip Analysis",
     img: "/uber_2018_logo.png",
     desc: "Built a time-series ensemble model (XGBoost + Random Forest + GBRT) to forecast hourly Uber ride demand (MAPE: 9.52%, R²: 0.966), with API deployment using Flask",
-    code: "https://github.com/username/uber-forecast",
+    code: "https://github.com/AKXLR8/uber-Api",
     demo: "https://uber-trip-predictor.vercel.app/",
   },
   {
